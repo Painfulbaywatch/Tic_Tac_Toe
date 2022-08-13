c66d1c67978a96d0b74900162e0a07824d7bb48b
Painfulbaywatch <andrewwillis@myop.ac.nz>
2022-08-13 18:24:19 +1200

    created methods board() and isDraw()

--- Tic_Tac_Toe/src/Board.java
+++ Tic_Tac_Toe/src/Board.java
@@ -12,26 +12,30 @@ public class Board {
 	/** Constructor to create the game board */
 	public Board() {
 		
-	 //TODO: initialise the cells array using ROWS and COLS constants 
-
-		
+	  //initialising the cells array with the values held in the static variables
+		cells = new Cell[GameMain.ROWS][GameMain.COLS];
 		for (int row = 0; row < GameMain.ROWS; row++) {
 			for (int col = 0; col < GameMain.COLS; col++) {
 				cells[row][col] = new Cell(row, col);
 			}
 		}
+		
 	}
 	
 
 	 /** Return true if it is a draw (i.e., no more EMPTY cells) */ 
 	public boolean isDraw() {
 		 
-		// TODO: Check whether the game has ended in a draw. 
-		// Hint: Use a nested loop (see the constructor for an example). Check whether any of the cells content in the board grid are Player.Empty. If they are, it is not a draw.
-		// Hint: Return false if it is not a draw, return true if there are no empty positions left
-		   
+			//checking each cell, if there is an empty cell the game cannot be a draw as there is till one or more moves to be made
+		   for(int row = 0; row < GameMain.ROWS; row++) {
+			   for(int col = 0; col < GameMain.COLS; col++) {
+				   if(cells[row][col].content == Player.Empty) {
+					   return false;
+				   }
+			   }
+		   }
 		
-
+		   return true;
 		
 	}
 	
